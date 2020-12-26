9295bf68e8f6ab8454e3fa818f7d070de9928e15
Zombieland4942 <zombieland4942@hotmail.com>
2020-12-26 12:15:00 +1000

    - Added compatability with the Creative Mod Infinite Void Tech - Tested Compatability with the Mega Research Test Tech

--- prototypes/update-tech.lua
+++ prototypes/update-tech.lua
@@ -4,12 +4,27 @@ for x, tech in pairs(data.raw["technology"]) do
         
         for y, ingredient in pairs(tech.unit.ingredients) do
 
-            local ingredient_name = data.raw["technology"][x].unit.ingredients[y][1]
-            
             data.raw["technology"][x].unit.ingredients[y][1] = "deadlock-stack-" .. data.raw["technology"][x].unit.ingredients[y][1]
 
-        end    
+        end   
     end
 end
 
 --log(serpent.block(tech))
+
+
+if mods["creative-mod"] then
+    
+    --UpdateTech("creative-mod_void-technology")
+
+    log(serpent.block(data.raw["technology"]["creative-mod_void-technology"]))
+
+    tech = data.raw["technology"]["creative-mod_void-technology"]
+
+    for y, ingredient in pairs(tech.unit.ingredients) do
+
+        data.raw["technology"][tech.name].unit.ingredients[y].name = "deadlock-stack-" .. data.raw["technology"][tech.name].unit.ingredients[y].name
+
+    end   
+
+end
